275b18512dd4b1702061cad9b209c7abac9a5679
ARDUINO, M5, C++, Pi, Access Databases <76819375+SF1960@users.noreply.github.com>
2025-02-12 16:53:28 +0000

    Update arduino_secrets.h
    removed personal data

--- arduino_secrets.h
+++ arduino_secrets.h
@@ -1,3 +1,3 @@
-#define SECRET_DEVICE_KEY "AcXCc?b7opivf@AZSYkGXzCt0"
-#define SECRET_OPTIONAL_PASS "nr5jfPxrvxmw"
-#define SECRET_SSID "VM3988748_LJ"
+#define SECRET_DEVICE_KEY "yourdevicekey"
+#define SECRET_OPTIONAL_PASS "yourpasswird"
+#define SECRET_SSID "yourssid"
